3e14481f58d10a6ee6e5d52e35df5953676d33b5
JuHeonHa <neon.ha@devercorp.com>
2022-11-05 16:09:04 +0900

    update typeorm

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRespository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardRespository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
