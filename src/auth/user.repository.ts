33c8a44fdf6dbc9e4a8f7166322192523893ae4c
JuHeonHa <neon.ha@devercorp.com>
2022-11-06 13:42:19 +0900

    update

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,25 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 
-@EntityRepository()
-export class UserRepository extends Repository<User> {}
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Already Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
